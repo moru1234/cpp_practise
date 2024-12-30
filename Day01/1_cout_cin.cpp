a9c20aa8135b2f3480f13920c1b5f2bf465b4b45
moru1234 <56359845+moru1234@users.noreply.github.com>
2024-12-30 18:16:44 +0000

    Cin and cout code

---
+++ Day01/1_cout_cin.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+//cin=>scanf >> used for input =>The extraction operator (>>) is used to extract data from an input stream. It's commonly used with std::cin to read user input from the console.
+// cout=>printf << used for output  =>The insertion operator (<<) is used to send data to an output stream.
+
+int main()
+{
+	int value;
+	cout << "Hello world"<<endl;
+	cout << "Good Eveening";
+
+	cout << "Enter value";
+	cin >> value;
+	cout << "The printed value" << value << endl;
+
+}
